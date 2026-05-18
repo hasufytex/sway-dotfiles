6f9bc2ed82f11cfa76b7c1018320b1f313096da0
George <georgekamenov04@gmail.com>
2026-05-18 21:34:35 +0300

    changed: replace i3 mouse accel script with xorg conf

--- i3/.config/i3/scripts/disable_mouse_accel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Apply flat acceleration profile to all pointer devices
-xinput list --id-only | while read id; do
-    if xinput list-props "$id" 2>/dev/null | grep -q "libinput Accel Profile Enabled"; then
-        xinput set-prop "$id" "libinput Accel Profile Enabled" 0 1
-    fi
-done
