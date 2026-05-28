3ae5a3ee1f53da06cc2106812a5564e92d7b6760
George <georgekamenov04@gmail.com>
2026-05-28 23:42:54 +0300

    changed: toggle_theme flips freedesktop color-scheme

--- scripts/.local/bin/toggle_theme.sh
+++ scripts/.local/bin/toggle_theme.sh
@@ -16,6 +16,8 @@ ln -sf "$HOME/my-i3-dotfiles/i3/.config/i3/theme-${NEW}.conf" \
 ln -sf "$HOME/my-i3-dotfiles/kitty/.config/kitty/theme-${NEW}.conf" \
        "$HOME/my-i3-dotfiles/kitty/.config/kitty/theme.conf"
 
+gsettings set org.gnome.desktop.interface color-scheme "prefer-${NEW}"
+
 i3-msg reload
 
 kill -SIGUSR1 $(pidof kitty) 2>/dev/null || true
